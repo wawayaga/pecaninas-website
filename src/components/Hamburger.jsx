5b2f5ba718dce4ef7b51aa6deea4697794b0ac94
wawayaga <wawas.studio@gmail.com>
2025-07-22 21:58:23 +0200

    Change links in hamburger menu to links from wouter

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import { Link } from "wouter";
 import Hamburger from "hamburger-react";
 
 export default function HamburgerComponent() {
   const [open, setOpen] = useState(false);
   return (
-    <div className="md:invisible p-3">
+    <div className="md:invisible">
       <Hamburger color="#224dbe" size={30} toggled={open} toggle={setOpen} />
       {open && (
         <div className="fixed absolute top-0 left-0 w-screen h-screen text-black bg-white">
@@ -15,21 +16,11 @@ export default function HamburgerComponent() {
             className="open-hamburger flex flex-col gap-10 items-center
             justify-center h-screen text-3xl text-center"
           >
-            <a id="open-hamburger" href="">
-              Services
-            </a>
-            <a id="open-hamburger" href="">
-              Booking
-            </a>
-            <a id="open-hamburger" href="">
-              About me
-            </a>
-            <a id="open-hamburger" href="">
-              Testimonials
-            </a>
-            <a id="open-hamburger" href="">
-              Contact
-            </a>
+            <Link href="/Services">Services</Link>
+            <Link href="/Booking">Booking</Link>
+            <Link href="/About-me">About me</Link>
+            <Link href="/Testimonials">Testimonials</Link>
+            <Link href="/Contact">Contact</Link>
           </div>
         </div>
       )}
